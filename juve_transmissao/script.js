67d36b9fabe18a1c2c77aa23af79fc0f870bf065
Point Media <50424643+andersongarciapinheiro@users.noreply.github.com>
2024-06-07 17:09:49 -0300

    asas

--- juve_transmissao/script.js
+++ juve_transmissao/script.js
@@ -1,1 +1,7 @@
-// Adicione a funcionalidade de JavaScript aqui, se necessário
+let lines = document.querySelectorAll(".product")
+
+lines.forEach((i, index) => {
+    if(!Number.isInteger(index/2)) {
+        i.style.backgroundColor = '#d1d1d1'
+    } 
+})
