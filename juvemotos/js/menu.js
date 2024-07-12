3e61876911d400cd7f76cad0bd62f8a8c7e29a4c
Anderson Garcia <anderson@pointmedia.com.br>
2024-07-12 12:15:44 -0300

    update

--- juvemotos/js/menu.js
+++ juvemotos/js/menu.js
@@ -6,11 +6,11 @@ onscroll = () => {
   if(scrollY > 10) {
     header.style.backgroundColor = '#222222f1'
     header.style.boxShadow = '#222222f1 0 0 2px 10px'
-    header.style.marginTop = '0px'
+    // header.style.marginTop = '0px'
     
   } else {
     header.style.backgroundColor = '#000000a1'
     header.style.boxShadow = '#000000a1 0 0 2px 10px'
-    header.style.marginTop = '20px'
+    // header.style.marginTop = '10px'
   }
 }
