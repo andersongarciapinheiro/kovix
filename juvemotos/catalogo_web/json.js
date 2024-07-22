840cae57a3ad23a5b83484970839b0c2f673a6e0
Anderson Garcia <anderson@pointmedia.com.br>
2024-07-22 15:09:15 -0300

    Update json.js

--- juvemotos/catalogo_web/json.js
+++ juvemotos/catalogo_web/json.js
@@ -1,107 +1,29 @@
 export const catalogData = {
   "Travas": [
     { "img": "./assets/travas/kd6.png", "nome": "Trava de disco", "linha": "KD6", "modelo": "Com alarme" },
-    // { "img": "./assets/travas/kd6.png", "nome": "KD6", "linha": "KD6", "modelo": "Preto" },
-    // { "img": "./assets/travas/kd6.png", "nome": "KD6", "linha": "KD6", "modelo": "Aço Escovado" },
-    // { "img": "./assets/travas/kd6.png", "nome": "KD6", "linha": "KD6", "modelo": "Verde Limão" },
-    // { "img": "./assets/travas/kd6.png", "nome": "KD6", "linha": "KD6", "modelo": "Laranja Fluorescente" },
-    // { "img": "./assets/travas/kd6.png", "nome": "KD6", "linha": "KD6-Y", "modelo": "Amarelo" },
-
     { "img": "./assets/travas/kaz10.png", "nome": "Trava de disco", "linha": "KAZ10", "modelo": "Com alarme" },
-    // { "img": "./assets/travas/kaz10.png", "nome": "KAZ10", "linha": "KAZ10", "modelo": "Aço Escovado" },
-    // { "img": "./assets/travas/kaz10.png", "nome": "KAZ10", "linha": "KAZ10", "modelo": "Verde Fluorescente" },
-    // { "img": "./assets/travas/kaz10.png", "nome": "KAZ10", "linha": "KAZ10", "modelo": "Amarelo" },
-    // { "img": "./assets/travas/kaz10.png", "nome": "KAZ10", "linha": "KAZ10", "modelo": "Laranja Fluorescente" },
-
     { "img": "./assets/travas/kal10.jpg ", "nome": "Trava de disco", "linha": "KAL10", "modelo": "Com alarme" },
-    // { "img": "./assets/travas/kaz10.png", "nome": "KAL10", "linha": "KAL10", "modelo": "Aço Escovado" },
-    // { "img": "./assets/travas/kaz10.png", "nome": "KAL10", "linha": "KAL10", "modelo": "Verde Fluorescente" },
-    // { "img": "./assets/travas/kaz10.png", "nome": "KAL10", "linha": "KAL10", "modelo": "Amarelo" },
-    // { "img": "./assets/travas/kaz10.png", "nome": "KAL10", "linha": "KAL10", "modelo": "Laranja Fluorescente" },
-
     { "img": "./assets/travas/kt6.png", "nome": "Trava de disco", "linha": "KT6", "modelo": "Com alarme" },
-    // { "img": "./assets/travas/kt6.png", "nome": "KT6", "linha": "KT6-BK", "modelo": "Preto" },
-    // { "img": "./assets/travas/kt6.png", "nome": "KT6", "linha": "KT6-BM", "modelo": "Aço Escovado" },
-    // { "img": "./assets/travas/kt6.png", "nome": "KT6", "linha": "KT6-FG", "modelo": "Verde Limão" },
-    // { "img": "./assets/travas/kt6.png", "nome": "KT6", "linha": "KT6-FO", "modelo": "Laranja Fluorescente" },
-    // { "img": "./assets/travas/kt6.png", "nome": "KT6", "linha": "KT6-Y", "modelo": "Amarelo" },
-
     { "img": "./assets/travas/knn1.png", "nome": "Trava de disco", "linha": "KNN1", "modelo": "Com alarme" },
-    // { "img": "./assets/travas/knn1.png", "nome": "KNN1", "linha": "KNN1-FG", "modelo": "Verde Limão" },
-    // { "img": "./assets/travas/knn1.png", "nome": "KNN1", "linha": "KNN1-FO", "modelo": "Laranja Fluorescente" },
-
     { "img": "./assets/travas/knx10.png", "nome": "Trava de disco", "linha": "KNX10", "modelo": "Com alarme" },
-    // { "img": "./assets/travas/knx10.png", "nome": "KNX10", "linha": "KNX10-BM", "modelo": "Aço Escovado" },
-    // { "img": "./assets/travas/knx10.png", "nome": "KNX10", "linha": "KNX10-FG", "modelo": "Verde Fluorescente" },
-    // { "img": "./assets/travas/knx10.png", "nome": "KNX10", "linha": "KNX10-FO", "modelo": "Laranja Fluorescente" },
-    
     { "img": "./assets/travas/knx6.png", "nome": "Trava de disco", "linha": "KNX6", "modelo": "Com alarme" },
-    // { "img": "./assets/travas/knx6.png", "nome": "KNX6", "linha": "KNX6-BM", "modelo": "Aço Escovado" },
-    // { "img": "./assets/travas/knx6.png", "nome": "KNX6", "linha": "KNX6-FG", "modelo": "Verde Fluorescente" },
-    // { "img": "./assets/travas/knx6.png", "nome": "KNX6", "linha": "KNX6-FO", "modelo": "Laranja Fluorescente" },
-    // { "img": "./assets/travas/knx6.png", "nome": "KNX6", "linha": "KNX6-Y", "modelo": "Amarelo" },
-
     { "img": "./assets/travas/knx15.jpg", "nome": "Trava de disco", "linha": "KAS15", "modelo": "Com alarme" },
-
     { "img": "./assets/travas/ks6.png", "nome": "Trava de disco", "linha": "KS6", "modelo": "Com alarme" },
-    // { "img": "./assets/travas/ks6.png", "nome": "KS6", "linha": "KS6 - BM", "modelo": "Aço Escovado" },
-    // { "img": "./assets/travas/ks6.png", "nome": "KS6", "linha": "KS6 - FG", "modelo": "Verde Limão" },
-    // { "img": "./assets/travas/ks6.png", "nome": "KS6", "linha": "KS6 - FO", "modelo": "Laranja Fluorescente" },
-    // { "img": "./assets/travas/ks6.png", "nome": "KS6", "linha": "KS6 - Y", "modelo": "Amarelo" },
-
     { "img": "./assets/travas/kvz.png", "nome": "Trava de disco", "linha": "KVZ1", "modelo": "Com alarme" },
-    // { "img": "./assets/travas/kvz.png", "nome": "KVZ1", "linha": "KVZ1 - FG", "modelo": "Verde Limão" },
-    // { "img": "./assets/travas/kvz.png", "nome": "KVZ1", "linha": "KVZ1 - FO", "modelo": "Laranja Fluorescente" },
-
     { "img": "./assets/travas/kvz2.png", "nome": "Trava de disco", "linha": "KVZ2", "modelo": "Com alarme" },
-    // { "img": "./assets/travas/kvz2.png", "nome": "KVZ2", "linha": "KVZ2 - BM", "modelo": "Aço Escovado" },
-    // { "img": "./assets/travas/kvz2.png", "nome": "KVZ2", "linha": "KVZ2 - FG", "modelo": "Verde Limão" },
-    // { "img": "./assets/travas/kvz2.png", "nome": "KVZ2", "linha": "KVZ2 - FO", "modelo": "Laranja Fluorescente" },
-    // { "img": "./assets/travas/kvz2.png", "nome": "KVZ2", "linha": "KVZ2 - Y", "modelo": "Amarelo" },
-
     { "img": "./assets/travas/kgh.png", "nome": "KGH", "linha": "KGH-BK", "modelo": "Preto" },
-
     { "img": "./assets/travas/zhlcamuflado.png", "nome": "ZHL", "linha": "ZHL", "modelo": "Camuflado" },
-
     { "img": "./assets/travas/khlr.png", "nome": "KHL-R", "linha": "KHL-R", "modelo": "Cadeado Motocicleta" },
-
     { "img": "./assets/travas/granada.jpg", "nome": "AGM", "linha": "AGM", "modelo": "Granada" },
-
     { "img": "./assets/travas/ktl.png", "nome": "Trava Bicicleta - KTL", "linha": "KTL 14", "modelo": "150 BK" },
-    // { "img": "./assets/travas/ktl.png", "nome": "Trava Bicicleta - KTL", "linha": "KTL 14", "modelo": "150 SS" },
-    // { "img": "./assets/travas/ktl.png", "nome": "Trava Bicicleta - KTL", "linha": "KTL 16", "modelo": "210 BK" },
-    // { "img": "./assets/travas/ktl.png", "nome": "Trava Bicicleta - KTL", "linha": "KTL 16", "modelo": "210 SS" },
-
     { "img": "./assets/travas/kvh.png", "nome": "Cadeado Náutico - KVH", "linha": "KVH-96", "modelo": "96" },
-    // { "img": "./assets/travas/kvh.png", "nome": "Cadeado Náutico - KVH", "linha": "KVH88", "modelo": "88" },
-    
     { "img": "./assets/travas/ktl14.jpeg", "nome": "Cadeado - KC", "linha": "KTL - 14", "modelo": "Suporte" },
-
     { "img": "./assets/travas/kc003.jpg", "nome": "Cadeado - KC", "linha": "KC003", "modelo": "Suporte de Guidão" },
     { "img": "./assets/travas/kvh17.jpg", "nome": "Cadeado - KC", "linha": "KHV-17", "modelo": "Suporte" },
-
-    // { "img": "./assets/travas/xbl.png", "nome": "Cadeado Motocicleta, Liga de Aço - XBL", "linha": "XBL1", "modelo": "25 XENA" },
-    // { "img": "./assets/travas/xbl.png", "nome": "Cadeado Motocicleta, Liga de Aço - XBL", "linha": "XBL1", "modelo": "35 XENA" },
-    // { "img": "./assets/travas/xbl.png", "nome": "Cadeado Motocicleta, Liga de Aço - XBL", "linha": "XBL2", "modelo": "25 XENA" },
-    // { "img": "./assets/travas/xbl.png", "nome": "Cadeado Motocicleta, Liga de Aço - XBL", "linha": "XBL2", "modelo": "35 XENA" },
-
-    // { "img": "./assets/travas/xga.png", "nome": "Cadeado Motocicleta, Liga de Aço - XGA", "linha": "XGA", "modelo": "01 XENA" },
-    // { "img": "./assets/travas/xga.png", "nome": "Cadeado Motocicleta, Liga de Aço - XGA", "linha": "XGA", "modelo": "02 XENA" },
-    // { "img": "./assets/travas/xga.png", "nome": "Cadeado Motocicleta, Liga de Aço - XGA", "linha": "XGA", "modelo": "03 XENA" },
-
-    // { "img": "./assets/travas/cabosalarme.png", "nome": "Cabo Sem Alarme - XXA", "linha": "XXA", "modelo": "XXA-150" },
-    
     { "img": "./assets/travas/kpl12.png", "nome": "Cabo Sem Alarme - KCL10", "linha": "10-120", "modelo": "Corrente com alarme" },
-    // { "img": "./assets/travas/kpl12.png", "nome": "Cabo Sem Alarme - KCL10", "linha": "10-150", "modelo": "Corrente com alarme" },
-    // { "img": "./assets/travas/kpl12.png", "nome": "Cabo Sem Alarme - KCL10", "linha": "8-120", "modelo": "Corrente com alarme" },
-
     { "img": "./assets/travas/kwl24.png", "nome": "Cabo e Cadeado com Alarme - KWL24", "linha": "KWL24", "modelo": "24-110 BK" },
-
     { "img": "./assets/travas/kdm.png", "nome": "Sensor - KD-AM", "linha": "KD-AM", "modelo": "Sensor KD6-AM/KT6-AM" },
-
     { "img": "./assets/travas/kc005.png", "nome": "Bateria - KC", "linha": "KC005", "modelo": "Bateria" },
-
     { "img": "./assets/travas/kc002.png", "nome": "Corda - KC", "linha": "KC002", "modelo": "Verde Limão" },
-    // { "img": "./assets/travas/kc002.png", "nome": "Corda - KC", "linha": "KC002", "modelo": "Laranja" }
   ],
 }
