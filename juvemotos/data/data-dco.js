b16a3a7988e9a888194d5f8dd7dfeccc800cc300
Anderson Garcia <anderson@pointmedia.com.br>
2024-07-30 11:15:58 -0300

    UPDATE

--- juvemotos/data/data-dco.js
+++ juvemotos/data/data-dco.js
@@ -1,27 +1,39 @@
 export const data_dco = [
   {
-    thumb: '../assets/thumb/kovix.jpg',
+    thumb: '../assets/thumb/kovix_teste.jpg',
     cliente: 'KOVIX',
     descricao: 'Travas com alarme',
-    url: './kovix.html',
+    url: '../catalogo_web/index.html',
   },
   {
-    thumb: '../assets/thumb/oxford.jpg',
-    cliente: 'Oxford',
-    descricao: 'Lubrificantes',
-    url: 'oxford.html',
+    thumb: '../assets/thumb/kovix_teste2.jpg',
+    cliente: 'KOVIX',
+    descricao: 'Travas sem alarme',
+    url: '../catalogo_web/index.html',
   },
   {
-    thumb: '../assets/thumb/nexforce.jpg',
-    cliente: 'Nexforce',
-    descricao: 'Acessórios',
-    url: 'nexforce.html',
+    thumb: '../assets/thumb/kovix_teste3.jpg',
+    cliente: 'KOVIX',
+    descricao: 'Cadeados',
+    url: '../catalogo_web/index.html',
   },
   {
-    thumb: '../assets/thumb/yamalube.jpg',
-    cliente: 'Yamalube',
-    descricao: 'Lubrificantes',
-    url: 'yamalube.html',
+    thumb: '../assets/thumb/kovix_teste4.jpg',
+    cliente: 'KOVIX',
+    descricao: 'Travas de guidão',
+    url: '../catalogo_web/index.html',
+  },
+  {
+    thumb: '../assets/thumb/kovix_teste5.jpg',
+    cliente: 'KOVIX',
+    descricao: 'Correntes',
+    url: '../catalogo_web/index.html',
+  },
+  {
+    thumb: '../assets/thumb/kovix_teste6.jpg',
+    cliente: 'KOVIX',
+    descricao: 'Suporte e Acessórios',
+    url: '../catalogo_web/index.html',
   },
 ]
 
